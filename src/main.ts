635c55e5058eb026ccb0c3b1be600315c9cdea7a
Marcin Maciorowski <m.maciorowski1991@gmail.com>
2023-01-21 16:42:18 +0100

    working with auth module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = 3333;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
+  await console.log(
+    `Aplikacja wystartowała pod adresem http://localhost:${PORT}/`,
+  );
 }
 
 bootstrap();
